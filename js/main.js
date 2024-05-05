c8a7e888df7b7447f14525910b6bdf5c654a9a3b
Zeko706 <131792180+Zeko706@users.noreply.github.com>
2024-05-05 18:32:51 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+import HeaderFunc from "./header.js";
+import productsFunc from "./products.js";
+import searchFunc from "./search.js";
+
+// ! add product to localStorage start
+(async function () {
+  const photos = await fetch("../js/data.json");
+  const data = await photos.json();
+
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+  productsFunc(data);
+  searchFunc(data);
+})();
+// ! add product to localStorage end
+
+// ! add cartItems to localStorage start
+const cartItems = document.querySelector(".header-cart-count");
+
+cartItems.innerHTML = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart")).length
+  : "0";
+
+// ! add cartItems to localStorage end
+
+// ! modal dialog start
+const modalDialogDOM = document.querySelector(".modal-dialog");
+const btnCloseDialog = document.querySelector(".modal-dialog .modal-close");
+const modalContentDOM = document.querySelector(".modal-dialog .modal-content");
+
+btnCloseDialog.addEventListener("click", function () {
+  modalDialogDOM.classList.remove("show");
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.composedPath().includes(modalContentDOM)) {
+    modalDialogDOM.classList.remove("show");
+  }
+});
+
+setTimeout(() => {
+  modalDialogDOM.classList.add("show");
+}, 3000);
+
+// ! modal dialog end
